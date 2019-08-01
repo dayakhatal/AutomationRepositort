7fcaf8546e927ad573789da03ebc5c23038c74b2
Dayanand <dayakhatal@gmail.com>
2019-08-01 21:27:05 +0530

    Data types  and variables

---
+++ BasiCoreJava/src/variablesAndDataTypes/DataTypes.java
@@ -0,0 +1,35 @@
+package variablesAndDataTypes;
+
+public class DataTypes {
+
+	public static void main(String[] args) 
+	{
+		
+		
+		//integer
+		int age=30;
+		System.out.println(age);
+		
+		//Character
+		char gender='M';
+		
+		
+		//Decimal Values
+		double marks=74.66;
+		float mark=67.87f;
+		//Boolean
+		boolean status=true;
+		
+		//Strings
+		String name="Daya";
+		
+		
+		//For integer 
+		byte a=20;
+		short b=10;
+		int c=30;
+		
+		
+	}
+
+}
